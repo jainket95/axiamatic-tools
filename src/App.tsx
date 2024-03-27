f4dc3305671b875e7ff6fae931f0e3e4c241cf68
aniket jain <aniket1995jain@gmail.com>
2024-03-28 00:21:20 +0530

    removed the commented jsx from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,29 +38,6 @@ function App() {
 	const selectedProducts = products.filter((product) => product.isSelected);
 
 	return (
-		// <div className="container mx-auto w-[100vw] h-[100vh]">
-		// 	<Header />
-
-		// 	<div className="lg:flex :flex-row lg:items-center lg:justify-center w-full h-full lg:flex-col-reverse md:flex-col-reverse sm:flex-col-reverse md:items-start sm:items-start md:pt-[12.5rem] sm:pt-[12.5rem] lg:pt-0">
-		// 		<div className="lg:w-2/3 lg:mr-10 md:mr-2 md:w-full lg:pb-0  md:pb-14 sm:mr-2 sm:w-full sm:pb-14">
-		// 			<Products
-		// 				products={selectedProducts}
-		// 				removeProduct={handleProductToggle}
-		// 				productDisplayQuantity={numberOfProducts}
-		// 				handleAddProductBox={handleAddProductBox}
-		// 			/>
-		// 		</div>
-		// 		<div className="lg:w-1/3 h-full lg:flex lg:items-center md:w-2/3 lg:mr-10 lg:mb-0 lg:mt-0 md:mb-14 md:mt-[21rem] sm:w-2/3 sm:mb-14 sm:mt-[10rem]">
-		// 			<AddProducts
-		// 				products={searchProducts}
-		// 				handleSelect={handleProductToggle}
-		// 				handleSearch={handleProductSearch}
-		// 				productDisplayQuantity={numberOfProducts}
-		// 				inputRef={inputRef}
-		// 			/>
-		// 		</div>
-		// 	</div>
-		// </div>
 		<div className="container mx-auto w-[100vw] h-[100vh]">
 			<Header />
 
