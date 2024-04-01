b976d3c7bde665f4c6482d7324b03a00b943f327
aniket jain <aniket1995jain@gmail.com>
2024-04-01 21:08:38 +0530

    added cursor class to TopHeader

--- client/src/components/TopHeader.tsx
+++ client/src/components/TopHeader.tsx
@@ -1,7 +1,9 @@
 const TopHeader = () => {
 	return (
 		<div className="w-[96%] flex justify-end items-center h-6">
-			<p className="font-semibold text-md text-gray-500 px-2">Exit Setup</p>
+			<p className="font-semibold text-md text-gray-500 px-2 cursor-pointer">
+				Exit Setup
+			</p>
 		</div>
 	);
 };
